e99130829c0d0edd13f4b2c0e463ca72f6cd9481
owaisikhan <owaiskhandawar310@gmail.com>
2026-04-02 01:05:57 +0500

    Co-authored-by: Ammar-Sagheer <Ammar-Sagheer@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+// git remote add origin https://github.com/your-username/your-repo.git
+// git branch -M main               # rename branch to main
+// git push -u origin main          # push and set upstream
+
+// echo "# fdsf" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Ammar-Sagheer/fdsf.git
+// git push -u origin main
