2f561b8cb3ddaa253849e65c5aab842550744f5e
Ammar <ammarsagheer@gmail.com>
2026-04-06 23:19:52 +0500

    Fixed Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,30 @@
 import Button from "../components/Button";
 import { useNavigate } from "react-router";
+
 function Home() {
   const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
     navigate("/menu");
   }
+
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex h-full items-center justify-center text-sm"
+      className="flex h-full w-full items-center justify-center text-sm"
     >
-      <div>
-        <p className="mb-6 text-xl font-bold tracking-wide uppercase md:text-3xl">
+      <div className="flex w-7/10 max-w-xl flex-col gap-4">
+        <p className="text-xl font-bold tracking-wide uppercase md:text-3xl">
           Welcome to react pizza %ammar
         </p>
 
-        <div className="flex items-center justify-between">
+        <div className="flex w-full items-center justify-between gap-2">
           <input
-            className="mx-2 w-7/10 rounded-2xl border-2 px-4 py-3 transition-all duration-700 focus:w-full"
+            className="w-7/10 rounded-2xl border-2 px-4 py-3 transition-all duration-700 focus:w-full"
             type="text"
             placeholder="enter name"
           />
-
           <Button>Login</Button>
         </div>
       </div>
