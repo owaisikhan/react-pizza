1b50a514f3910d06691ba3bbccaee4ddbd9c8ffe
owaisikhan <owaiskhandawar310@gmail.com>
2026-04-07 06:21:14 +0500

    Home page button styled

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 function Button({ children, onclick, to, type }) {
   const login =
-    "w-7/10 bg-blue-300 hover:bg-blue-400 opacity-25 hover:opacity-100 rounded-xl py-3 text-sm font-medium text-stone-900 transition-all duration-300";
+    "w-7/10 bg-blue-300 hover:bg-blue-400 opacity-25 hover:opacity-100 rounded-xl py-3 text-sm font-medium text-stone-900 transition-all duration-300 hover:shadow-lg hover:shadow-blue-400/50";
 
   const base =
     "text-md rounded-3xl border-2 border-amber-600 bg-amber-500 px-4 py-2 font-bold text-stone-800 hover:bg-amber-700 md:rounded-3xl md:p-3 md:text-sm";
